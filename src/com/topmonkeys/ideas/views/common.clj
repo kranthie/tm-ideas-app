cf1b9107d5b1af13cf2b5e8029a61e8f37f129b1
Kranthi Muppala <kranthi.muppala@gmail.com>
2011-11-01 21:54:37 +0000

    added google analytics to the common layout.

--- src/com/topmonkeys/ideas/views/common.clj
+++ src/com/topmonkeys/ideas/views/common.clj
@@ -33,7 +33,19 @@
    [:meta { :property "fb:app_id" :content "306009122744195" }]
    [:script {:src "http://connect.facebook.net/en_US/all.js#xfbml=1"}]
    [:title title]
-   (map #(get includes %) include-files)])
+   (map #(get includes %) include-files)
+   "<script type=\"text/javascript\">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-253140-2']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+</script>"
+   ])
 
 ; Builds html footer.
 (defpartial build-footer []
